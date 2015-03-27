e254a99f90e77d8dc0bda627049bc173fe1f69b1
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-03-27 13:03:39 +0300

    Bug: Fix on login_validation() function to notice public and members

--- app/application/controllers/main.php
+++ app/application/controllers/main.php
@@ -157,41 +157,59 @@ class Main extends CI_Controller {
 	/*
 	*Validate log ins
 	*/
-	public function login_validation(){
-		$this->load->library('form_validation');
-		$this->form_validation->set_rules('id_number', 'ID number', 'required|trim|xss_clean|callback_validate_credentials');
-		$this->form_validation->set_rules('password', 'Password', 'required|sha1|trim');
+	public function login_validation()
+	{
+		
+		if(!$this->session->userdata("is_logged_in"))
+		{
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules('id_number', 'ID number', 'required|trim|xss_clean|callback_validate_credentials');
+			$this->form_validation->set_rules('password', 'Password', 'required|sha1|trim');
 
-		if ($this->form_validation->run()){
-			/*get username*/
-			$id_number = $this->input->post("id_number");
-			$user_data = $this->model_users->user_details("first_name", $id_number);
-			$username = ucfirst($this->array_to_single($user_data, "first_name"));
-			
-			//create session for the user
-			$data = array(
-					"id_number" => $id_number,
-					"is_logged_in" => 1,
-					"username" => $username
-				);
+			if ($this->form_validation->run())
+			{
+				/*get username*/
+				$id_number = $this->input->post("id_number");
+				$user_data = $this->model_users->user_details("first_name", $id_number);
+				$username = ucfirst($this->array_to_single($user_data, "first_name"));
+				
+				//create session for the user
+				$data = array(
+						"id_number" => $id_number,
+						"is_logged_in" => 1,
+						"username" => $username
+					);
 
-			$data_2 = array(
-					"main" => "members",
-					"title" => "Naxxserian &middot; Members"
-				);
+				$data_2 = array(
+						"main" => "members",
+						"title" => "Naxxserian &middot; Members"
+					);
 
-			$this->session->set_userdata($data);
-			#redirect('main/members');
-			$this->_load_view($data_2);
-		} 
+				$this->session->set_userdata($data);
+				#redirect('main/members');
+				$this->_load_view($data_2);
+			}
+		 
+			else
+			{
+				$data = array(
+						"main" => "login",
+						"title" => "Naxxserian &middot; Login"
+					);
+
+				$this->_load_view($data);
+			}
+		}
 		else
 		{
+			/*not logged in*/
 			$data = array(
-					"main" => "login",
-					"title" => "Naxxserian &middot; Login"
-				);
+						"main" => "members",
+						"title" => "Naxxserian &middot; Members"
+					);
 
-			$this->_load_view($data);
+				$this->_load_view($data);
+
 		}
 	}
 
--- app/application/views/members.php
+++ app/application/views/members.php
@@ -18,31 +18,31 @@
 		<!-- Personal info -->
 		<h4><i class="fa fa-user"></i>Personal Information</h4>
 		<label for="first_name">First Name:</label> 
-		<input type="text" id="first_name" class="col-lg-12">
+		<input type="text" id="first_name" class="col-lg-12 form-control">
 		<br><br><br><br>
 
 		<label for="middle_name">Middle Name:</label> 
-		<input type="text" id="middle_name" class="col-lg-12">
+		<input type="text" id="middle_name" class="col-lg-12 form-control">
 		<br><br><br><br>
 
-		<label for="surname">Surname:</label> 
-		<input type="text" id="surname" class="col-lg-12">
-		<br><br><br><br>
+		<label for="surname">Surname:</label>  
+		<input type="text" id="surname"class="col-lg-12 form-control"> 
+		<br><br><br><br> 
 		<hr class="hrDividerBetween">
 
 		<!-- Change password -->
 		<h4><i class="fa fa-lock"></i>Change Password</h4>
-		<form method="POST">
+		<form method="POST" class="form-group">
 			<label for="old_password">Old Password</label> 
-			<input type="text" id="old_password/" class="col-lg-12">
+			<input type="text" id="old_password" class="col-lg-12 form-control">
 			<br><br><br><br>
 
 			<label for="new_password">New password</label> 
-			<input type="text" id="new_password" class="col-lg-12">
+			<input type="text" id="new_password" class="col-lg-12 form-control">
 			<br><br><br><br>
 
 			<label for="conf_new_password">Confirm New password</label> 
-			<input type="text" id="conf_new_password" class="col-lg-12">
+			<input type="text" id="conf_new_password" class="col-lg-12 form-control">
 			<br><br><br><br>
 
 			<input type='submit' name='change_password_btn' value='Change Password' class='btn btn-danger'/>
