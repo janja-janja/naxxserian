d7a40c99d7825264c19d391679efd902ecf09381
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-03-30 19:33:16 +0300

    Code: Auth and Public controllers created

---
+++ app/application/controllers/auth.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+/*
+Authorised members function helpers only
+*/
+{
+
+}
---
+++ app/application/controllers/public.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Public extends CI_Controller
+/*
+Public |Non-Authorised members function helpers only
+*/
+{
+
+	
+}
