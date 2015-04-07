0417649b04c3ebac26fb5d5b2ac361ca1794bdd1
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-04-07 15:04:13 +0300

    Code: base URL UPDATED

--- app/application/config/config.php
+++ app/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/CI_apps/naxxserian/app';
+$config['base_url']	= 'http://localhost/naxxserian/app';
 
 /*
 |--------------------------------------------------------------------------
