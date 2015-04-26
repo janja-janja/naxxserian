7d898d6af62ae128a486a8996d9e6dbc0bf86a44
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-04-26 21:46:07 +0300

    Code: Finance view update

--- app/application/views/finance.php
+++ app/application/views/finance.php
@@ -6,4 +6,19 @@
 	
 	<h4>Naxxserian Financials</h4>
 
+	<div>
+		<h4 class="btn btn-success">My contributions</h4>
+		<h4 class="btn btn-info">All contributions</h4>
+	</div>
+
+	<div id="personal-contributions" class="offset-top">
+		<h4>Personal Contributions</h4>
+
+	</div>
+
+	<div id="all-contributions" style="display:none" class="offset-top">
+		<h4>Group Contributions</h4>
+
+	</div>
+
 </div>
