2184988f8d3eae981858e3e06c35cf1e9aa702ec
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-04-28 08:15:14 +0300

    Code: Admin->__load_view() helper added
    To help load views with initialized data e.g page_title and other variables that might seem necessary to pass

--- app/application/controllers/admin.php
+++ app/application/controllers/admin.php
@@ -6,12 +6,26 @@ Class to hold admin helpers for CRUDL processes
 C->reate, R->ead, U->pdate, D->elete, L->
 */
 {
+	private $data;
+
 	function __construct()
 	{
 		parent::__construct();
 
 		#default timezone
 		date_default_timezone_set("Africa/Nairobi");
+
+		#set default models here
+
+		#default data
+		$data = array(
+				"title" => "Naxxserian Admin Panel"
+			);
+	}
+
+	public function __load_view($data)
+	{
+		$this->load->view("inc/admin_template", $data);
 	}
 
 	public function add_user()
