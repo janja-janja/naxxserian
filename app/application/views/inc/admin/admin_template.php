758c7b2d444c62045526caab89495d146d28c913
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-04-28 08:28:19 +0300

    Code: admin header&footer added
    templates initialized to display header, data and footer in that order

--- app/application/views/inc/admin/admin_template.php
+++ app/application/views/inc/admin/admin_template.php
@@ -1,7 +1,11 @@
 <?php
 
 #load admin header
+$this->load->view("inc/admin/admin_header");
+
 
 #load data from controller
+$this->load->view($admin);
 
 #load admin footer
+$this->load->view("inc/admin/admin_footer");
