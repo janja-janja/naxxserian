5a5bb174e74f5849c81689f3290b70db3b0255e4
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-04-28 10:17:21 +0300

    Code: admins_model added
    Model to hold admin roles and functions of the admin.

---
+++ app/application/models/admin/admins_model.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Admins_model extends CI_Model
+/*
+Holds admins(user) helper functions to assist admin roles
+*/
+{
+	public function admin_can_log_in($role)
+	/*
+	check if a member can log in as an admin
+	@params -> string(role)
+	@return boolean
+	*/
+	{
+		
+	}
+}
