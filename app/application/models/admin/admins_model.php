9ebc050f6cecb9ad5a6c1fde6c715d3164d74531
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-04-28 10:34:06 +0300

    Code: Admins_model->can_log_in(x) added
    Verify if the member has access to the admin control panel

--- app/application/models/admin/admins_model.php
+++ app/application/models/admin/admins_model.php
@@ -5,13 +5,30 @@ class Admins_model extends CI_Model
 Holds admins(user) helper functions to assist admin roles
 */
 {
-	public function admin_can_log_in($role)
+	public function can_log_in($category)
 	/*
 	check if a member can log in as an admin
-	@params -> string(role)
+	@params -> int(category)
 	@return boolean
 	*/
 	{
-		
+		$condition = array(
+				"id_number" => $this->input->post("id_number"),
+				"password" => $this->input->post("password"),
+				"category" => $category
+			);
+
+		$this->db->where($condition);
+
+		$query = $this->db->get("members");
+
+		if($query->num_rows() == 1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
