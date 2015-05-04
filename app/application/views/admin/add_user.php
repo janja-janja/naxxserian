f4f75effa7c3522ad765fe20491cb092b00b2534
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-04 11:52:11 +0300

    Code: add_user view update
    New member field details added for admin to capture essential details

--- app/application/views/admin/add_user.php
+++ app/application/views/admin/add_user.php
@@ -1,3 +1,42 @@
 <div class="container offset-top">
-	<h3 class="highlighter-text">Users</h3>
+
+	<div class="col-md-offset-3">
+		<h4 class="col-md-3 btn btn-success">Add New Members</h4>
+		<h4 class="col-md-3 btn btn-warning">Edit Details</h4>
+		<h4 class="col-md-3 btn btn-danger">Delete Member</h4>
+	</div>
+	<br><br><br>
+	<form method="POST" class="well col-md-10 col-md-offset-1" action="<?php echo base_url(); ?>admin_add_content/submit">
+
+      <h4 class="highlighter-text">Member details</h4>
+      <hr class="hrDividerBetween">
+      <label for="first_name">First Name</label>
+      <input type="text" class="col-lg-12 form-control" name="first_name" placeholder="First Name"/>
+      
+      <br><br><br><br>
+      
+      <label for="middle_name">Middle Name</label>
+      <input type="text" class="col-lg-12 form-control" name="middle_name" placeholder="Middle Name"/>
+
+      <br><br><br><br>
+      <label for="surname">Surname</label>
+      <input type="text" class="col-lg-12 form-control" name="surname" placeholder="Surname" />
+
+      <br><br><br><br>
+      <label for="email_address">Email Address</label>
+      <input type="text" class="col-lg-12 form-control" name="email_address" placeholder="Email Address" />
+
+      <br><br><br><br>
+      <label for="id_number">ID Number</label>
+      <input type="text" class="col-lg-12 form-control" name="id_number" placeholder="ID Number" />
+      <br><br><br>
+       <hr class="hrDividerBetween">
+
+      <input type="submit" value="Register Member" class="btn btn-success btn-lg">
+      
+			
+		</div>
+		
+	</form>
+
 </div>
